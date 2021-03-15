c3421d9057625d2d26e1c4683b2bf7f5dc700367
Steven Faican <safaican.fie@unach.edu.ec>
2021-03-15 13:48:04 -0500

    sss

--- static/js/indexS.js
+++ static/js/indexS.js
@@ -63,7 +63,7 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	document.getElementById("temp").innerHTML=message.payloadString.split(" ")[2] [1]
+	document.getElementById("temp").innerHTML=message.payloadString.split(" ")[2] 
 	document.getElementById("hum").innerHTML=message.payloadString.split(" ")[1]
 	document.getElementById("ppm").innerHTML=message.payloadString.split(" ")[3]
 	document.getElementById("his").innerHTML=message.payloadString.split(" ")[4]
