e1c755e1a67af3361ca6dcb26478b39d7b3d8757
Steven Faican <safaican.fie@unach.edu.ec>
2021-03-15 13:42:14 -0500

    sss

--- static/js/indexS.js
+++ static/js/indexS.js
@@ -60,14 +60,13 @@ function LED1_Off(){
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
-
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	document.getElementById("temp").innerHTML=message.payloadString.split("=")[2]
-	document.getElementById("hum").innerHTML=message.payloadString.split("=")[1]
-	document.getElementById("ppm").innerHTML=message.payloadString.split("=")[3]
-	document.getElementById("his").innerHTML=message.payloadString.split("=")[4]
+	document.getElementById("temp").innerHTML=message.payloadString.split(" ")[2] [1]
+	document.getElementById("hum").innerHTML=message.payloadString.split(" ")[1]
+	document.getElementById("ppm").innerHTML=message.payloadString.split(" ")[3]
+	document.getElementById("his").innerHTML=message.payloadString.split(" ")[4]
 	
 }
    
