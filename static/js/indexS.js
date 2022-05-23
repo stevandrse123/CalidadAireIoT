30b784031e001c8dad3d01a57828afea95a7fa4c
Steven Faican <safaican.fie@unach.edu.ec>
2022-05-23 12:59:14 -0500

    2022

--- static/js/indexS.js
+++ static/js/indexS.js
@@ -30,7 +30,7 @@ function LED1_Off(){
   var options = {
    useSSL: false,
     userName: "stevandres123@gmail.com",
-    password: "esoestodo1994",
+    password: "Sickdog22As",
     onSuccess:onConnect,
     onFailure:doFail
   }
