ff599b4cb44a18e93fc839be8a333b6a89b4cbe1
Steven Faican <safaican.fie@unach.edu.ec>
2022-05-24 13:20:50 -0500

    actual mqqt

--- static/js/indexS.js
+++ static/js/indexS.js
@@ -30,7 +30,7 @@ function LED1_Off(){
   var options = {
    useSSL: false,
     userName: "stevandres123@gmail.com",
-    password: "Sickdog22As",
+    password: "esoestodo1994",
     onSuccess:onConnect,
     onFailure:doFail
   }
